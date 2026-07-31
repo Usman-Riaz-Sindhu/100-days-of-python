7bf2fa6b2e756893987f72dfe293875c6a5aa299
Usman Riaz Sindhu <usmanriazsandhu@gmail.com>
2026-07-31 11:05:03 +0500

    lecture 31: Randomization and Python Lists

--- Day 4/31.py
+++ Day 4/31.py
@@ -1,3 +1,7 @@
 import random
-my_num = random.randint(1, 10)
-print("my_num is:", my_num)
+
+# my_num = random.randint(1, 10)
+# print("my_num is:", my_num)
+
+number2 = random.random()  # generates a random float between 0 and 1
+print("number2 is:", number2)
