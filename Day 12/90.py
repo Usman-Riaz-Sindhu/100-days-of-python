a0319734932720dcd3ce4e8106d6e68ebf2e72a5
Usman Riaz Sindhu <usmanriazsandhu@gmail.com>
2026-08-14 17:52:38 +0500

    Day 12 update

--- Day 12/90.py
+++ Day 12/90.py
@@ -3,9 +3,3 @@
 # PI = 3.14 #constant variable
 # GOOGLE_URL = "https://www.google.com" #constant variable
 
-def a_function(a_parameter):
-    a_variable = 15
-    return a_parameter
- 
-a_function(10)
-print(a_variable)
