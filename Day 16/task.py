0c1f0c1316d366c29dc4930692b25161b1d8fc3f
Usman Riaz Sindhu <usmanriazsandhu@gmail.com>
2026-08-20 10:36:26 +0500

    Day 16

---
+++ Day 16/task.py
@@ -0,0 +1,21 @@
+# import turtle
+from turtle import Turtle, Screen
+# this works same as above but the only difference is now we dont need to call by turtle.Turtle we can simply do timy = Turtle()
+
+timy = Turtle()
+print(timy)
+timy.shape("turtle")
+timy.color("DarkGreen") 
+timy.speed(2)
+timy.forward(200)   # aagay 100 pixels
+timy.backward(50)   # peechay 50 pixels
+
+timy.right(90)      # right 90° turn
+timy.left(90)       # left 90° turn
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
